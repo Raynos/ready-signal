88a237240c86899e56aab6f0eda3505c79559558
Joshua T Corbin <jcorbin@wunjo.org>
2015-02-22 20:41:44 -0800

    <3 ;s

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-module.exports = Ready
+module.exports = Ready;
 
 function Ready() {
-    var ready = false
-    var listeners = []
-    var args
+    var ready = false;
+    var listeners = [];
+    var args;
 
-    onready.emit = onready
+    onready.emit = onready;
 
-    return onready
+    return onready;
 
     function onready(callback) {
         if (typeof callback !== "function" && !ready) {
-            ready = true
-            args = arguments
-            listeners.forEach(call)
-            return (listeners = [])
+            ready = true;
+            args = arguments;
+            listeners.forEach(call);
+            return (listeners = []);
         } else if (ready) {
-            return callback && call(callback)
+            return callback && call(callback);
         }
 
-        listeners.push(callback)
+        listeners.push(callback);
     }
 
     function call(cb) {
-        cb.apply(null, args)
+        cb.apply(null, args);
     }
 }
